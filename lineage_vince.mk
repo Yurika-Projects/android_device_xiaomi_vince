ab891820854aea6198d4fd84e2dfaae8f40a18bf
rahul <41164942+rk134@users.noreply.github.com>
2022-01-26 20:02:12 +0000

    vince: update build fingerprint to raven
    raven (Pixel 6 Pro), 5/01/2022 patch.

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -45,4 +45,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
