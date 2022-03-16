5f1e9f264e7fe0aa57380b7529809cececd9769f
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2022-03-16 09:53:41 +0700

    vince: create fluid-config.mk

---
+++ fluid-config.mk
@@ -0,0 +1,17 @@
+# Some common Fluid Flags
+
+# Gapps flags
+
+TARGET_INCLUDE_GAPPS := true
+
+TARGET_GAPPS_ARCH := arm64
+
+IS_PHONE := true
+
+# Whether or not target supports google recoder
+
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Build Type
+
+FLUID_BUILD_TYPE := UNOFFICIAL
