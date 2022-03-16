bb555f72a24e4abacbd6293a2f0b54d2d4859706
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2022-03-16 10:00:45 +0700

    vince: rebuild tree

--- fluid_vince.mk
+++ fluid_vince.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The FluidOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,8 +27,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common exTHmUI stuff.
-$(call inherit-product, vendor/fluid/config/common.mk)
+# Inherit some common Fluid stuff.
+$(call inherit-product, device/xiaomi/vince/fluid-config.mk)
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
